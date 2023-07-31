1d88c7edf65bec2343e494658ba2822b9967d90e
ihsanabuhanifah <ihsanabuhanifah@gmail.com>
2023-07-31 13:52:10 +0700

    tes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
+import Button from "./component/Button";
+import Latihan from "./component/Latihan";
+import Note from "./component/Note";
 function Home() {
   return (
-    <h1>Hello World</h1>
-  )
+    <>
+      <h1>Hello World</h1>
+
+      <Latihan name={"izan"} username="izan" age={20} isVerified={true} />
+
+      {/* <Latihan name={'hilmi'} username="hilmi" age={25} isVerified />
+      <Latihan name={'zaky'} username="zaky" age={20} isVerified ={false} />    */}
+      <Button title="save" isDisabled />
+      <Button title="cancel" />
+      <Note title="Belajar NextJS" status="success">
+        <p>Saya sedang bealjar NextJS</p>
+      </Note>
+      <Note title="Belajar NextJS" status="success">
+        <div className="text-green-500">Saya sedang bealjar NextJS</div>
+      </Note>
+      <Note title="Belajar NextJS" status="success">
+        <Button title="cancel" />
+      </Note>
+      <Note title="Belajar NextJS" status="success">
+        ok
+      </Note>
+    </>
+  );
 }
+
+export default Home;
