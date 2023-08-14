e2c61305a16b9703caa4486e21fc90ff23d5d8ff
ihsanabuhanifah <ihsanabuhanifah@gmail.com>
2023-08-14 09:56:07 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,98 @@
 "use client"; // gunakan use client karena ada onChange pda komponen
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import BelajarState from "./module/belajarState";
 import Card from "./latihan/Card";
 import Button from "./component/Button";
 import InputText from "./component/InputText";
+import Select from "./component/Select";
+import Label from "./component/Label";
 
-export type Identitas = {
-  nama: string;
-  sekolah: string;
-  umur: number | null;
-};
-
-export type Hasil = {
-  mata_pelajaran: string;
-  nilai: number | null;
-};
+interface Value {
+  title: string;
+  author: string;
+  year: number | string;
+}
 
 const Home = () => {
-  let [tanggal, setTanggal] = useState(0);
-  let [bulan, setBulan] = useState("Agustus");
+  const [values, setValues] = useState<Value>({
+    title: "",
+    author: "",
+    year: "",
+  });
+
+  const handleChange = (event: ChangeEvent<any>) => {
+    setValues((prevValues) => {
+      return {
+        ...prevValues,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = (e: ChangeEvent<any>) => {
+    e.preventDefault();
+
+    alert(JSON.stringify(values));
+  };
 
   return (
     <main className="space-y-5">
-      <h1>Latihan</h1>
-      <Card
-        bulan={bulan}
-        tanggal={tanggal}
-        setTanggal={setTanggal}
-        setBulan={setBulan}
-      />
-      <Button
-        onClick={() => {
-          setTanggal((c) => c + 1);
-        }}
-        colorSchema="blue"
-        title="tambah"
-      />
-      <Button
-        onClick={() => {
-          setTanggal((c) => c - 1);
-        }}
-        colorSchema="red"
-        title="kurang"
-      />
-      <InputText
-        id="bulan"
-        name={"bulan"}
-        value={bulan}
-        onChange={(e) => {
-          setBulan(e.target.value);
-        }}
-      />
+      {JSON.stringify(values)}
+      <h1 className="text-red-500 font-bold">Latihan Form</h1>
+      <form onSubmit={handleSubmit} className="space-y-3">
+        <section>
+          <Label htmlFor="author" isRequired title="Penulis" />
+          <InputText
+            placeholder="Nama Penulis buku"
+            id="author"
+            name="author"
+            value={values.author}
+            onChange={handleChange}
+          />
+        </section>
+
+        <section>
+          <Label htmlFor="title" isRequired title="Judul Buku" />
+          <InputText
+            placeholder="Nama Judul buku"
+            id="title"
+            name="title"
+            value={values.title}
+            onChange={handleChange}
+          />
+        </section>
+        <section>
+          <Label htmlFor="year" title="Tahun Terbit" />
+          <Select
+            value={values.year}
+            name="year"
+            onChange={handleChange}
+            isError
+            id="year"
+            options={[
+              {
+                value: 2020,
+                label: "2020",
+              },
+              {
+                value: 2021,
+                label: "2021",
+              },
+              {
+                value: 2022,
+                label: "2022",
+              },
+              {
+                value: 2023,
+                label: "2023",
+              },
+            ]}
+          />
+        </section>
+        <section>
+          <Button type="submit" colorSchema="blue" title="Simpan" />
+        </section>
+      </form>
     </main>
   );
 };
